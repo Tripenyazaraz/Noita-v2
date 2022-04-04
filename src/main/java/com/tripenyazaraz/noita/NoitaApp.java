6c6f2ad9613a8f6d112e9edb506b946acc3017ca
Eldar Allakhyarov <tripenyazaraz@gmail.com>
2022-04-04 19:08:39 +0600

    проиграл битву с xml, снес его. начало ui

--- src/main/java/com/tripenyazaraz/noita/NoitaApp.java
+++ src/main/java/com/tripenyazaraz/noita/NoitaApp.java
@@ -1,25 +1,75 @@
 package com.tripenyazaraz.noita;
 
 import com.tripenyazaraz.noita.engine.Engine;
+import com.tripenyazaraz.noita.particle.powder.Sand;
+import com.tripenyazaraz.noita.ui.ParticleButton;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.tripenyazaraz.noita.engine.Engine.FIELD_HEIGHT;
+import static com.tripenyazaraz.noita.engine.Engine.FIELD_WIDTH;
+import static java.lang.Math.round;
 
 public class NoitaApp extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(NoitaApp.class.getResource("noita.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), Engine.FIELD_WIDTH, Engine.FIELD_HEIGHT);
-        stage.setTitle("Noita");
-        stage.setScene(scene);
-        stage.show();
-    }
+    Engine engine = new Engine();
 
     public static void main(String[] args) {
         launch();
-//        Engine engine = new Engine();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Noita");
+        stage.setScene(createGUI());
+        stage.show();
+    }
+
+    private Scene createGUI() {
+        List<ParticleButton> buttons = createButtons();
+
+        VBox sidebar = new VBox();
+        sidebar.setSpacing(30);
+        sidebar.setAlignment(Pos.TOP_CENTER);
+        sidebar.getChildren().addAll(buttons);
+
+        Canvas canvas = new Canvas(FIELD_WIDTH+320, FIELD_HEIGHT);
+
+        EventHandler<MouseEvent> onMousePressedEventHandler = t -> {
+            double x = t.getSceneX();
+            double y = t.getSceneY();
+            engine.getField().putParticle(new Sand((int) round(x), (int) round(y)));
+        };
+
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, onMousePressedEventHandler);
+
+        HBox rootBox = new HBox(canvas, sidebar);
+        return new Scene(rootBox);
+    }
+
+    private List<ParticleButton> createButtons() {
+        try {
+            Path path = Path.of("data/particles");
+            List<String> lines = Files.readAllLines(path);
+            List<ParticleButton> buttons = new ArrayList<>();
+            for (String str : lines){
+                buttons.add(new ParticleButton(str));
+            }
+            return buttons;
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/main/java/com/tripenyazaraz/noita/engine/Engine.java
+++ src/main/java/com/tripenyazaraz/noita/engine/Engine.java
@@ -2,10 +2,10 @@ package com.tripenyazaraz.noita.engine;
 
 public class Engine {
 
-    public static final int FIELD_WIDTH = 1420;
+    public static final int FIELD_WIDTH = 1080;
     public static final int FIELD_HEIGHT = 1080;
 
-    public static final Field field = new Field(10, 10);
+    public static final Field field = new Field(FIELD_WIDTH, FIELD_HEIGHT);
 
     public Field getField() {
         return field;
