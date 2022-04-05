9edb4b484a9ce6f66bfb6b2343037c18028c54eb
Eldar Allakhyarov <tripenyazaraz@gmail.com>
2022-04-05 11:20:23 +0600

    forget class

---
+++ src/main/java/com/tripenyazaraz/noita/ui/ParticleButton.java
@@ -0,0 +1,13 @@
+package com.tripenyazaraz.noita.ui;
+
+import javafx.scene.control.Button;
+
+public class ParticleButton extends Button {
+
+    public ParticleButton(String name) {
+        super(name);
+        this.setWidth(20);
+        this.setHeight(10);
+    }
+
+}
